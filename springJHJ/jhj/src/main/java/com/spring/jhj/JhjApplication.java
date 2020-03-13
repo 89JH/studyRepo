76caeff2075657b70c082fc5f6f76f1b9c519782
JHJ <jhdb.kr@gmail.com>
2020-03-13 19:11:12 +0900

    test

---
+++ springJHJ/jhj/src/main/java/com/spring/jhj/JhjApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jhj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhjApplication.class, args);
+	}
+
+}
---
+++ springJHJ/jhj/src/test/java/com/spring/jhj/JhjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jhj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
